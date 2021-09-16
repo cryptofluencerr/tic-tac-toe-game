0626c794cbf4c40a64eef8aa57c7bb36c839e2e4
cryptofluencerr <89958405+cryptofluencerr@users.noreply.github.com>
2021-09-16 18:39:47 +0530

    need to edit reset button function

--- index.js
+++ index.js
@@ -7,3 +7,19 @@ const b6 = document.querySelector("#b6").value;
 const b7 = document.querySelector("#b7").value;
 const b8 = document.querySelector("#b8").value;
 const b9 = document.querySelector("#b9").value;
+const resetEl = document.querySelector("#reset-el")
+
+resetEl.addEventListener("click", function myFuncReset() {
+// It is used to refresh the page
+location.reload();
+b1.value = "";
+b2.value = "";
+b3.value = "";
+b4.value = "";
+b5.value = "";
+b6.value = "";
+b7.value = "";
+b8.value = "";
+b9.value = "";
+return console.log("hi");
+}
